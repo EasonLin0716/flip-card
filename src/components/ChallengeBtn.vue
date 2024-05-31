0136f213e1111b20a8574e615be30a1b02e9ed64
EasonLin0716 <uu9924079@gmail.com>
2024-05-31 11:49:31 +0800

    [refactor] move challenge btn styles

--- src/components/ChallengeBtn.vue
+++ src/components/ChallengeBtn.vue
@@ -25,10 +25,67 @@ const startGameHandler = () => {
     emits('startGame');
 };
 </script>
-
 <template>
     <button :class="{
-        'content-memory__play-game__btn--disabled': isPlaying,
-        'content-memory__play-game__btn': true
+        'is-disabled': isPlaying,
+        'challenge-btn': true
     }" @click="startGameHandler">{{ buttonText }}</button>
 </template>
+<style lang="scss" scoped>
+@import '../styles/_variables.scss';
+@import '../styles/_mixins.scss';
+
+.challenge-btn {
+    position: relative;
+    display: block;
+    margin: 48px auto 0 auto;
+    font-size: 36px;
+    font-weight: bold;
+    padding: 12px 40px;
+    border-radius: 36.5px;
+    border: 0;
+    background-color: $challengeBgColor;
+    box-shadow: 0 10px 0 0 $Blue-700;
+    cursor: pointer;
+    transition: all 0.2s ease-out;
+    top: 0;
+    color: #fff;
+
+    @include pc-mm {
+        font-size: 32px;
+        padding: 10px 36px;
+        margin-top: 32px;
+    }
+
+    @include pc-s {
+        margin-top: 24px;
+    }
+
+    @include ipad-s {
+        font-size: 24px;
+        padding: 10px 29px;
+        box-shadow: 0 7px 0 0 $Blue-700;
+    }
+
+    &:hover {
+        box-shadow: 0 4px 0 0 $Blue-700;
+        top: 9px;
+
+        @include ipad-s {
+            top: 4px;
+        }
+    }
+
+    &.is-disabled {
+        pointer-events: none;
+        background-color: $Blue-500;
+        color: #fff;
+        top: 0;
+        box-shadow: 0 12px 0 0 $Blue-400;
+
+        @include ipad-s {
+            box-shadow: 0 3px 0 0 $Blue-400;
+        }
+    }
+}
+</style>
