ef3ed9124cb0e7d73411abf170337201ea63b37e
Eason <uu9924079@gmail.com>
2024-06-03 22:08:13 +0800

    [test] add 3 tests to ChallengeBtn

--- src/components/__tests__/ChallengeBtn.spec.ts
+++ src/components/__tests__/ChallengeBtn.spec.ts
@@ -1,16 +1,34 @@
-import { expect, test, describe } from "vitest";
+import { expect, test, describe, beforeEach } from "vitest";
 import { VueWrapper, mount } from "@vue/test-utils";
 import { PLAY_STATE_TEXT, PLAY_STATE } from "../../constants";
 import ChallengeBtn from "../ChallengeBtn.vue";
 
+let wrapper: VueWrapper;
+
+beforeEach(() => {
+  wrapper = mount(ChallengeBtn, {
+    props: { playState: PLAY_STATE.NOT_STARTED },
+  });
+});
+
 describe("ChallengeBtn", () => {
-    const wrapper: VueWrapper = mount(ChallengeBtn);
-    test(`Renders ${PLAY_STATE_TEXT.NOT_STARTED}`, async () => {
-        await wrapper.setProps({ playState: PLAY_STATE.NOT_STARTED });
-        expect(wrapper.text()).toContain(PLAY_STATE_TEXT.NOT_STARTED);
-    });
-    test(`Renders ${PLAY_STATE_TEXT.PLAYING}`, async () => {
-        await wrapper.setProps({ playState: PLAY_STATE.PLAYING });
-        expect(wrapper.text()).toContain(PLAY_STATE_TEXT.PLAYING);
-    })
+  test('renders a button element with class name "challenge-btn"', () => {
+    expect(wrapper.find("button").classes()).toContain("challenge-btn");
+  });
+  test(`renders ${PLAY_STATE_TEXT.NOT_STARTED}`, async () => {
+    await wrapper.setProps({ playState: PLAY_STATE.NOT_STARTED });
+    expect(wrapper.text()).toContain(PLAY_STATE_TEXT.NOT_STARTED);
+  });
+  test(`renders ${PLAY_STATE_TEXT.PLAYING}`, async () => {
+    await wrapper.setProps({ playState: PLAY_STATE.PLAYING });
+    expect(wrapper.text()).toContain(PLAY_STATE_TEXT.PLAYING);
+  });
+  test('has class name "disabled" when playState is PLAYING', async () => {
+    await wrapper.setProps({ playState: PLAY_STATE.PLAYING });
+    expect(wrapper.find("button").classes()).toContain("is-disabled");
+  });
+  test('emits "startGame" event when clicked', async () => {
+    await wrapper.find("button").trigger("click");
+    expect(wrapper.emitted()).toHaveProperty("startGame");
+  });
 });
