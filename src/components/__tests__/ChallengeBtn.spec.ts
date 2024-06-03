c358f611f8f176cac9cbd3c8e4b2f67b5c80ed4f
EasonLin0716 <uu9924079@gmail.com>
2024-06-03 13:47:20 +0800

    [test] update using constant

--- src/components/__tests__/ChallengeBtn.spec.ts
+++ src/components/__tests__/ChallengeBtn.spec.ts
@@ -1,16 +1,16 @@
 import { expect, test, describe } from "vitest";
 import { VueWrapper, mount } from "@vue/test-utils";
-import { PLAY_STATE_TEXT } from "../../constants";
+import { PLAY_STATE_TEXT, PLAY_STATE } from "../../constants";
 import ChallengeBtn from "../ChallengeBtn.vue";
 
 describe("ChallengeBtn", () => {
     const wrapper: VueWrapper = mount(ChallengeBtn);
     test(`Renders ${PLAY_STATE_TEXT.NOT_STARTED}`, async () => {
-        await wrapper.setProps({ playState: 0 });
+        await wrapper.setProps({ playState: PLAY_STATE.NOT_STARTED });
         expect(wrapper.text()).toContain(PLAY_STATE_TEXT.NOT_STARTED);
     });
     test(`Renders ${PLAY_STATE_TEXT.PLAYING}`, async () => {
-        await wrapper.setProps({ playState: 1 });
+        await wrapper.setProps({ playState: PLAY_STATE.PLAYING });
         expect(wrapper.text()).toContain(PLAY_STATE_TEXT.PLAYING);
     })
 });
