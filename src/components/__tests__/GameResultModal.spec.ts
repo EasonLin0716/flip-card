43c53935276636f7c69ecc9a6ea84d5dce71b51c
EasonLin0716 <uu9924079@gmail.com>
2024-06-06 10:48:53 +0800

    [test] add LoseGameModal and GameResultModal test

--- src/components/__tests__/GameResultModal.spec.ts
+++ src/components/__tests__/GameResultModal.spec.ts
@@ -1,19 +1,15 @@
 import { expect, test, describe, beforeEach } from "vitest";
 import { VueWrapper, mount } from "@vue/test-utils";
-import LoseGameModal from "../LoseGameModal.vue";
+import GameResultModal from "../GameResultModal.vue";
 
 let wrapper: VueWrapper;
 
 beforeEach(() => {
-  wrapper = mount(LoseGameModal);
+  wrapper = mount(GameResultModal);
 });
 
-describe("LoseGameModal", () => {
+describe("GameResultModal", () => {
     test('renders a section element with class name "content-modal"', () => {
         expect(wrapper.find("section").classes()).toContain("content-modal");
-    });
-    test('emits "replay" when replay button is clicked', () => {
-        wrapper.find("button").trigger("click");
-        expect(wrapper.emitted()).toHaveProperty("replay");
     });
 });
